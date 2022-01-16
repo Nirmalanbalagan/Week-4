14bc3b25e3c28821c30d3f6034f72f5b9cd18f1a
Nirmalanbalagan <97286362+Nirmalanbalagan@users.noreply.github.com>
2022-01-16 15:30:23 +0530

    Update Classroom1.java

--- Classroom1.java
+++ Classroom1.java
@@ -1,10 +1,25 @@
 package Week4.day;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class Classroom1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver=new ChromeDriver();
+		driver.get("https://erail.in");
+		driver.manage().window().maximize();
+			
+		driver.findElement(By.id("txtStationFrom")).clear();
+		driver.findElement(By.xpath("//div[@title='Chennai Egmore']")).click();
+		
+driver.findElement(By.id("txtStationTo")).clear();		
+driver.findElement(By.xpath("//div[@title='Madurai Jn']")).click();
+driver.findElement(By.id("chkSelectDateOnly")).clear();
+driver.findElement(By.id("buttonFromTo")).click();
 	}
-
 }
+© 2022 GitHub, Inc.
